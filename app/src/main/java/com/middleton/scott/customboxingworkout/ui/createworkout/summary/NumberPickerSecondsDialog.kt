82a5d3544894b6558e324dfdb02ea96bc6b56a15
scott <mrscottmiddleton@gmail.com>
2020-09-17 21:07:19 +0100

    time to complete

--- app/src/main/java/com/middleton/scott/customboxingworkout/ui/createworkout/summary/NumberPickerSecondsDialog.kt
+++ app/src/main/java/com/middleton/scott/customboxingworkout/ui/createworkout/summary/NumberPickerSecondsDialog.kt
@@ -8,25 +8,27 @@ import android.view.WindowManager
 import androidx.annotation.Nullable
 import androidx.fragment.app.DialogFragment
 import com.middleton.scott.commandMeBoxing.R
-import kotlinx.android.synthetic.main.dialog_save_combination.*
+import kotlinx.android.synthetic.main.dialog_number_picker_mins_secs.*
+import kotlinx.android.synthetic.main.dialog_save_combination.save_btn
 
-
-class SaveCombinationDialog(
-    private val onSave: ((String) -> Unit),
-    private val onDelete: (() -> Unit)
+class NumberPickerSecondsDialog(
+    private val seconds: Int,
+    private val onSave: ((Int) -> Unit),
+    private val onCancel: (() -> Unit)
 ) : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.dialog_save_combination, container)
+        return inflater.inflate(R.layout.dialog_number_picker_secs, container)
     }
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        secs_np.maxValue = 59
+        secs_np.minValue = 0
+        secs_np.value = seconds
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        // Show soft keyboard automatically and request focus to field
-        name_et!!.requestFocus()
         dialog?.window?.setSoftInputMode(
             WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
         )
@@ -35,25 +37,13 @@ class SaveCombinationDialog(
 
     private fun setClickListeners() {
         save_btn.setOnClickListener {
-            onSave(name_et.text.toString())
+            onSave(secs_np.value)
             dismiss()
         }
 
-        delete_btn.setOnClickListener {
-            onDelete()
+        cancel_btn.setOnClickListener {
+            onCancel()
             dismiss()
-        }
-
-        time_to_complete_et.setOnClickListener {
-            var secs = 0
-            if(!time_to_complete_et.text.isNullOrBlank()){
-                secs = time_to_complete_et.text.toString().toInt()
-            }
-            NumberPickerSecondsDialog(secs, {
-                time_to_complete_et.setText(it.toString())
-            }, {
-
-            }).show(childFragmentManager, "")
         }
     }
 }
